e2aa57809ad3259cfd4103f797574af924438f7b
Mujadded Al Rabbani Alif <Mujadded.alif@gmail.com>
2023-11-06 12:38:06 +0000

    initated

---
+++ record.py
@@ -0,0 +1,35 @@
+import cv2
+from datetime import datetime
+from picamera2 import Picamera2
+
+# Frame shape
+_FRAME_WEIGHT, _FRAME_HEIGHT = 1024, 768
+
+
+def start(callback_function):
+   # Initiating camera instance
+  picam2 = Picamera2()
+  picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (_FRAME_WEIGHT, _FRAME_HEIGHT)}))
+  picam2.start()
+  
+  # Define the codec and create VideoWriter object
+  fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
+  video_name= f"./captured_video/{datetime.today().strftime('%Y%m%d%H%M%S')}.avi"
+
+  # FPS for recording video. Setting 6 as the real fps is 6 to 7
+  fps = 30.0
+  # Video Recorder instance
+  out = cv2.VideoWriter(video_name,fourcc, fps, (_FRAME_WEIGHT, _FRAME_HEIGHT))
+  
+  while(True):
+    # Grab frame from video stream
+    image = picam2.capture_array()
+    
+     # Converting the image from 640x480x4 to 640x480x3
+    output_image=cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+    out.write(output_image)
+    
+    callback_function(output_image)
+  
+    
+
---
+++ stream_and_record.py
@@ -0,0 +1,57 @@
+import cv2
+from flask import Flask, render_template, Response
+import threading
+from record import start
+
+outputFrame = None
+lock = threading.Lock()
+
+# initialize a flask object
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+def render_image(image):
+    # grab global references to the video stream, output frame, and
+    # lock variables
+    global outputFrame, lock
+    with lock:
+        outputFrame = image.copy()
+
+def call():
+    """The Thread Call"""
+    start(render_image)
+
+def gen():
+    global outputFrame, lock
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__': 
+    # start a thread that will perform motion detection
+    t = threading.Thread(
+        target=call, 
+    )
+    t.daemon = True
+    t.start()
+    app.run(host='0.0.0.0', port =5000, debug=False, threaded=True, use_reloader=False)
